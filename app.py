4ecc551cd00bfeed29f5295bbf0c8c22529cdea0
Vineet Palan <vineetpalan@users.noreply.github.com>
2019-08-13 21:22:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def handle_route(**kwargs):
 if __name__ == '__main__':
     mocked_response = read_mocked_response()
     for rule in mocked_response:
-        app.add_url_rule(rule, 'handle_route', handle_route, methods=['POST', 'GET'])
+        app.add_url_rule(rule, 'handle_route', handle_route, methods=list(mocked_response[rule].keys()))
     app.run(host='0.0.0.0')
